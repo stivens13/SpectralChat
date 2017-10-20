77be6d4300597b2dc025f574851bf9b39b7603db
stivens73 <stivens54@gmail.com>
2017-10-20 11:12:23 -0700

    .gitignore

--- SpeactralChatProto/SpeactralChatProto/ViewController.swift
+++ SpeactralChatProto/SpeactralChatProto/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
