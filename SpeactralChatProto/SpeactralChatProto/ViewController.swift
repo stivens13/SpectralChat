720c1f8af13c7e88ce1ed849a580efa1e39acb78
stivens73 <stivens54@gmail.com>
2017-10-20 12:43:03 -0700

    trying to speak to firebase

--- SpeactralChatProto/SpeactralChatProto/ViewController.swift
+++ SpeactralChatProto/SpeactralChatProto/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var button: UIButton!
     
+    @IBAction func handleSend(sender: AnyObject) {
+        let ref: DatabaseReference!
+        ref = Database.database().reference()
+        ref.child("message").setValue(textField.text)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let ref: DatabaseReference!
+//        let ref = Database.database().reference()
+//        ref.observeEventType
         // Do any additional setup after loading the view, typically from a nib.
     }
 
