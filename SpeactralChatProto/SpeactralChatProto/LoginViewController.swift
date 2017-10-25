071685c3306dcacdca867be6cba6656808d9753c
John Mai <mai.john294@gmail.com>
2017-10-24 19:08:06 -0700

    Added new class so the project to handle logins. Right now it needs to be implemented

---
+++ SpeactralChatProto/SpeactralChatProto/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  SpeactralChatProto
+//
+//  Created by John Mai on 10/24/17.
+//  Copyright © 2017 Nikita Voloshenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class LoginViewController:UIViewController {
+    
+}
