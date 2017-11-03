0cc1783784be9862f5765411d585129b5695a039
John Mai <mai.john294@gmail.com>
2017-11-02 18:35:47 -0700

    Added JSQMessagesViewController to the project. Will continue to work on it tonight. Need to make some basic changes to the console as well

---
+++ SpeactralChatProto/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  SpeactralChatProto
+//
+//  Created by John Mai on 11/2/17.
+//  Copyright © 2017 Nikita Voloshenko. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+#import <JSQMessagesViewController/JSQMessages.h>
+
+#endif /* Bridging_Header_h */
---
+++ SpeactralChatProto/SpeactralChatProto/ChatViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChatViewController.swift
+//  SpeactralChatProto
+//
+//  Created by John Mai on 11/2/17.
+//  Copyright © 2017 Nikita Voloshenko. All rights reserved.
+//
+
+import Foundation
