8b9bfc14b1eb43a592543f27f0c7c9deee6f742b
John Mai <mai.john294@gmail.com>
2017-11-15 16:51:02 -0800

    Added SwiftKeychainWrapper library to project

---
+++ SpeactralChatProto/SpeactralChatProto/ConversationListVC.swift
@@ -0,0 +1,15 @@
+//
+//  ConversationListVC.swift
+//  SpeactralChatProto
+//
+//  Created by John Mai on 11/14/17.
+//  Copyright © 2017 Nikita Voloshenko. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import SwiftKeychainWrapper
+
+class ConversationListVC: UIViewController {
+    
+}
