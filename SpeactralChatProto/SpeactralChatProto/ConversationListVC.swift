db8230fa54da1619c39cb8bde9cdf251c5dd1375
John Mai <mai.john294@gmail.com>
2017-11-17 12:18:49 -0800

    Working on ConversationListVC

--- SpeactralChatProto/SpeactralChatProto/ConversationListVC.swift
+++ SpeactralChatProto/SpeactralChatProto/ConversationListVC.swift
@@ -10,6 +10,27 @@ import Foundation
 import Firebase
 import SwiftKeychainWrapper
 
-class ConversationListVC: UIViewController {
+class ConversationListVC: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return MessageDetailCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     
 }
---
+++ SpeactralChatProto/SpeactralChatProto/MessageDetailCell.swift
@@ -0,0 +1,59 @@
+//
+//  MessageDetailCell.swift
+//  SpeactralChatProto
+//
+//  Created by John Mai on 11/15/17.
+//  Copyright © 2017 Nikita Voloshenko. All rights reserved.
+//
+import UIKit
+import Foundation
+import Firebase
+import FirebaseStorage
+import FirebaseDatabase
+import SwiftKeychainWrapper
+
+class MessageDetailCell: UITableViewCell {
+    
+    @IBOutlet weak var recipientImg: UIImageView!
+    @IBOutlet weak var recipientName: UILabel!
+    @IBOutlet weak var chatPreview: UILabel!
+    
+    var messageDetail: MessageDetail!
+    var userPostKey: DatabaseReference!
+    let currentUser = KeychainWrapper.standard.string(forKey: "uid")
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func configureCell(messageDetail: MessageDetail) {
+        self.messageDetail = messageDetail
+        let recipientData = Database.database().reference().child("users").child(messageDetail.recipient)
+        
+        recipientData.observeSingleEvent(of: .value, with: { (snapshot) in
+            let data = snapshot.value as! Dictionary<String, AnyObject>
+            let username = data["username"]
+            let userImg = data["userImg"]
+            self.recipientName.text = username as? String
+            
+            let ref = Storage.storage().reference(forURL: user as! String)
+            
+            ref.data(withMaxSize: 100000, completion: {(data, error)
+                if error != nil {
+                    print("could not load image")
+                } else {
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.recipientImg.image = img
+                        }
+                    }
+                }
+            })
+        })
+    }
+}
+
