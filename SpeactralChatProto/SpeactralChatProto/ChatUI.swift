975188efc60fe6dbc879025e75070e419e7b7fdc
John Mai <mai.john294@gmail.com>
2017-11-28 17:33:18 -0800

    Got messages to display on the ChatUI. Conversation needs to be stored in Firebase

--- SpeactralChatProto/SpeactralChatProto/ChatUI.swift
+++ SpeactralChatProto/SpeactralChatProto/ChatUI.swift
@@ -21,11 +21,36 @@ class ChatUI: JSQMessagesViewController {
     override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
         print("didPressSend")
         print("\(text)")
-        
+        messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text))
+        collectionView.reloadData()
+        print(messages)
     }
     
     override func didPressAccessoryButton(_ sender: UIButton!) {
         print("didPressAccessory")
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        
+        return messages[indexPath.item]
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        return cell;
+    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        return bubbleFactory?.outgoingMessagesBubbleImage(with: UIColor.blue)
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
