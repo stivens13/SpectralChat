ac1ba334da69ac7ebbdaf6f1828dd9273ac41515
John Mai <mai.john294@gmail.com>
2017-12-22 11:29:17 -0800

    Check

---
+++ SpeactralChatProto/SpeactralChatProto/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  SpeactralChatProto
+//
+//  Created by Nikita Voloshenko on 12/22/17.
+//  Copyright © 2017 Nikita Voloshenko. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
